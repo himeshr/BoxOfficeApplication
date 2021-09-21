6812ba2274fd5cace5cdfd19c597b993a9994aaa
himeshr <72493481+himeshr@users.noreply.github.com>
2021-09-21 16:42:30 +0530

    Make code changes to perform conditional async operation in SoldTicketsInfoPrinterConsumer.accept()

--- src/main/java/com/shows/boxoffice/core/SoldTicketsInfoPrinterConsumer.java
+++ src/main/java/com/shows/boxoffice/core/SoldTicketsInfoPrinterConsumer.java
@@ -1,10 +1,14 @@
 package com.shows.boxoffice.core;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import org.springframework.stereotype.Component;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.shows.boxoffice.config.AppConstants;
+import com.shows.boxoffice.data.SeatType;
 import com.shows.boxoffice.data.SoldTicketsInfo;
 
 @Component
@@ -12,6 +16,14 @@ public class SoldTicketsInfoPrinterConsumer implements Consumer<SoldTicketsInfo>
 
 	@Override
 	public void accept(SoldTicketsInfo sti) {
-		System.out.println("Received tickets info for movie: " + sti.getMovieName());
+		if(sti.getSeatType().equals(SeatType.Gold)) {
+			CompletableFuture.runAsync(() -> {
+				Uninterruptibles.sleepUninterruptibly(1000,
+						TimeUnit.MILLISECONDS);
+				System.out.println(String.format(
+						"Asynchronously sent %s class tickets info for movie: %s ", 
+						sti.getSeatType(), sti.getMovieName()));
+		    });
+		}
 	}
 }
